00e083ab95b19e9cdcac47811913685c7c750348
Slim Shady <asterix1515@hotmail.com>
2026-04-24 16:18:41 -0400

    new edit

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export const metadata = {
 export default function Home() {
   return (
     // <main className="min-h-screen bg-black text-white max-w-3xl mx-auto">
-    <main className="min-h-screen bg-black text-white max-w-full mx-auto">
+    <main className="min-h-screen bg-black text-white max-w-screen mx-auto">
       <ScoreBoard />
     </main>
   );
