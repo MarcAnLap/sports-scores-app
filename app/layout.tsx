a9899d15cff79a741c25174f3a75fc581f74d5c4
Slim Shady <asterix1515@hotmail.com>
2026-04-25 14:27:29 -0400

    new edit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -29,11 +30,20 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Script
+        {/* <Script
     id="microsoft-clarity"
     src="https://www.clarity.ms/tag/whbcl3x1gx"
     strategy="afterInteractive"
-  />
+  /> */}
+  <Script id="microsoft-clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "whbcl3x1gx");
+  `}
+</Script>
         {children}
         </body>
     </html>
